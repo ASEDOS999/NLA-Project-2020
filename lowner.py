c395e3667d852382c2830a2ce5d793043e01d638
Garbuz Victor <garbouz.victor@gmail.com>
2020-12-14 23:25:22 +0300

    gradient was changed

--- lowner.py
+++ lowner.py
@@ -1,74 +1,121 @@
 import numpy as np
 from numpy.linalg import norm, matrix_rank, eig, inv, cholesky, svd
 from numpy import sqrt
+import sys
+import torch
+
+if not sys.warnoptions:
+    import os, warnings
+
+    warnings.simplefilter("error")
+    os.environ["PYTHONWARNINGS"] = "error"
+
 
 ## The gradient of l_p-regression at c
-def grad_compute(A, c, p, d):
-    grad = np.zeros((d,))
-    factors = np.zeros((d,))
-    for i in range(d): # то что в элементах градиента в знаменателях
-        factors[i] = np.abs(np.dot(A[i, :], c))
-    for j in range(d): # заполняем вектор который в выражении градиента
-        grad[j] = np.sum(A[j, :] / factors) * c[j]
-    grad *= norm(np.matmul(A, c), p) ** (p-1) * norm(np.matmul(A, c), p-1) ** (p-1)
-    return grad
+def grad_compute(A, x, p):
+    A_ = torch.tensor(A, requires_grad=False)
+    x_ = torch.tensor(x.astype(np.float64), requires_grad=True)
+    y_ = torch.matmul(A_, x_)
+    y_ = torch.norm(y_, p)
+    y_.backward()
+    return x_.grad.detach().numpy()
 
 
-def lowner(A, p):
+def lowner(A, p, max_iter):
+    iter_num = 0
     d = A.shape[1]
-    #E := A ball centered around the origin which contains L
-    r = 1/sqrt(norm(A, 2))   #3 r \leq 1/max(\sigma)
-    F = r * np.identity(d) 
-    c = np.zeros(shape=(d,)) 
+    # E := A ball centered around the origin which contains L
+    # r = 100 / (norm(A, -2) ** (1/p))
+    r = 1/norm(A, -2)
+    F = r * np.identity(d)
+    F_prev = F
+    c = np.zeros(shape=(d,))
     n = A.shape[0]
-    while True: 
+    stop = False
+    while True:
+        iter_num += 1
         ## 7-12
-        while norm(np.matmul(A, c), p) > 1: ## c not in L
-            grad = grad_compute(A, c, p, d)
-            H = 1/norm(grad, np.inf) * grad
-            b = 1/sqrt(np.matmul(np.transpose(H), np.matmul(F, H))) * np.matmul(F, H)
-            c = c - 1/(d + 1) * b
-            F = (d ** 2)/(d ** 2 - 1) * (F - 2/(d + 1) * np.matmul(b, np.transpose(b)))
+        while norm(np.matmul(A, c), p) > 1:  ## c not in L
+            grad = grad_compute(A, c, p)
+            H = 1 / np.max(np.abs(grad)) * grad
+            const = H.T @ F @ H
+            b = 1 / sqrt(const) * np.matmul(F, H)
+            c = c - 1 / (d + 1) * b
+            F = (d ** 2) / (d ** 2 - 1) * (F - 2 / (d + 1) * np.outer(b, b.T))
         ## 13 - 16
         containd = True
-        w, v = eig((np.identity(d) + c).T @ inv(F) @ (np.identity(d) + c))
+        w, v = eig(inv(F.astype(np.float64)) * d ** 2)
+        v = v.real.astype(np.float64)
+        w = w.real.astype(np.float64)
+        if not (w > 0).all():
+            print("не положительно определена стала на очередной итерации")
+            stop = True
+        if stop:
+            break
         for ind, val in enumerate(w):
-            v[ind] /= sqrt(val)
+            v[ind] = v[ind] / sqrt(val)
+        #     v[ind] = (v[ind]) + c
+        containd = True
         for vec in v:
-            if norm(np.matmul(A, vec/np.sqrt(d) - c), p) >= 1:
+            if norm(np.matmul(A, vec), p) > 1:
+                containd = False
+            if norm(np.matmul(A, 2 * c - vec), p) > 1:
                 containd = False
         if containd:
             break
-            
         ## 17
-        max_v = v[0]
-        _max = norm(np.matmul(A, v), p)
-        for vec in v:
-            temp = norm(np.matmul(A, vec), p)
-            if temp >= _max:
-                max_v = vec
-                _max = temp
+        max_index = np.argmax(abs(w))
+        max_v = v[max_index]
+        _max = norm(np.matmul(A, max_v), p)
+        for i, eigh_v in enumerate(v):
+            if norm(np.matmul(A, eigh_v), p) > _max:
+                _max = norm(np.matmul(A, eigh_v), p)
+                max_v = v[i]
+        # for debug
+        # if v[0][1] == 0:
+        #     max_v = v[1]
+        # _max = norm(np.matmul(A, max_v), p)
+
+        # for vec in v:
+        #     temp = norm(np.matmul(A, vec), p)
+        #     if temp >= _max:
+        #         max_v = vec
+        #         _max = temp
+        #
+        #     temp = norm(np.matmul(A, 2 * c - vec), p)
+        #     if temp >= _max:
+        #         max_v = 2 * c - vec
+        #         _max = temp
+
         v = max_v
         ##18..26
-        grad = grad_compute(A, v, p, d)
-        H = 1/norm(grad, np.inf) * grad
-        z = 1/(d + 1)/(d + 1)
-        sigma = (d**3)*(d+2)/((d+1)**3)/(d-1)
-        zeta = 1 + 1/2/(d**2)/((d+1) ** 2)
-        tau = 2/d/(d+1)
-        b = 1/sqrt(np.matmul(np.transpose(H), np.matmul(F, H))) * np.matmul(F, H)
-        F = zeta * sigma * (F - tau * np.outer(b, b))
-        c = c - z*b
+        grad = grad_compute(A, v, p)
+        H = 1 / max(abs(grad)) * grad
+        z = 1 / ((d + 1) * (d + 1))
+        sigma = (d * d * d) * (d + 2) / (((d + 1) ** 3) * (d - 1))
+        zeta = 1 + 1 / (2 * d * d * (d + 1) * (d + 1))
+        tau = 2 / (d * (d + 1))
+        const = H.T @ F @ H
+        b = 1 / sqrt(const) * np.matmul(F, H)
+        F_prev = F
+        F = zeta * sigma * (F - tau * np.outer(b, b.T))
+        c = c - z * b
+        if iter_num >= max_iter:
+            print("Max iteration exceeded!")
+            break
+    F = F_prev
     ##27..28
-    G = cholesky(inv(F))
+    G = cholesky(inv(F.astype(np.float32)))
     S, D, V = svd(G)
     D = np.diag(D)
+    print("iter", iter_num)
     return D, V
 
-def l_p_low_rank(A, k, p):
+
+def l_p_low_rank(A, k, p, max_iter=100):
     d = A.shape[1]
-    D, V = lowner(A, p)
-    U = np.matmul(A, inv(np.matmul(D, np.transpose(V))))
+    D, V = lowner(A, p, max_iter)
+    U = np.matmul(A, inv(np.matmul(D, V)))
     set_of_sigma = np.diag(D)
-    D_k = np.diag(np.array(list(set_of_sigma[0 : k]) + [0] * (d - k)) )
+    D_k = np.diag(np.array(list(set_of_sigma[0: k]) + [0] * (d - k)))
     return U, D_k, V, set_of_sigma
--- testLowner.py
+++ testLowner.py
@@ -10,10 +10,21 @@ class TestLowner(unittest.TestCase):
         #A = np.asarray([[1, 2, 0], [0, 1, 117], [-7, 0, 1]])
         A = np.random.rand(30, 20)
         k = 10
-        U, D_k, V, sigmas = l_p_low_rank(A, k, 1)
-        print(np.linalg.norm(A - U @ D_k @ V.T))
+        U, D_k, V, sigmas = l_p_low_rank(A, k, 1, 10)
+        print(np.linalg.norm(A - U @ D_k @ V))
+        U, D_k, V, sigmas = l_p_low_rank(A, k, 1, 100)
+        print(np.linalg.norm(A - U @ D_k @ V))
         U, D, V = np.linalg.svd(A, full_matrices=False)
         print(np.linalg.norm(A - U[:, :k] @ np.diag(D[:k]) @ V[:k, :]))
+
+    def test2(self):
+        A = np.asarray([[3.0, 5.0], [0.0, 1.0]])
+        k = 2
+        U, D_k, V, sigmas = l_p_low_rank(A, k, 1, 25)
+        print(np.linalg.norm(A - U @ D_k @ V))
+        U, D, V = np.linalg.svd(A, full_matrices=False)
+        print(np.linalg.norm(A - U[:, :k] @ np.diag(D[:k]) @ V[:k, :]))
+
 
 if __name__ == '__main__':
     unittest.amin()
